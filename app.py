3c6a70c6d35cbd35ac4db0c4376f32e0afc08e61
Yuqi Zhang <yz3983@columbia.edu>
2021-10-12 23:42:27 -0400

    app.py update

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import flask
 from flask import *
 #from application.user_services import userclass
 from application_services.address_service import *
-from application_services.user_service import *
 import json
  
 app = Flask(__name__)
@@ -19,35 +18,35 @@ def index_page():
 def users():
     if flask.request.method == 'POST':
         # User form['user'] for data insertion -> None
-
         insert_user(request.form['user'])
 
     elif flask.request.method == 'GET':
         # get_all_user_info() -> JSON()
-        return json.dumps(get_all_user())
+        return get_all_user()
 
 
 @app.route('/users/<userID>', methods=['GET', 'PUT', 'DELETE'])
 def users_id(userID):
     if flask.request.method == 'GET':
         # get_user_info(userID) - userID get from url -> JSON
-        return json.dumps(get_user_by_id(userID))
+        return get_user_by_id(userID)
 
     elif flask.request.method == 'PUT':
         # update_user_info(userID) - userID get from url - request.form['user'] input form
-        update_user(request.args.get('userID'), request.form['user'])
+        update_user(userID, request.form['user'])
 
     elif flask.request.method == 'DELETE':
         # delete_user_info(userID) - userID get from url
-        delete_user(request.args.get('userID'))
+        delete_user(userID)
+
 
 @app.route('/users/<userID>/address', methods=['GET', 'POST'])
-def users_id_address():
+def users_id_address(userID):
     if flask.request.method == 'POST':
-        return update_address_by_uid(request.args.get('userID'))
+        return update_address_by_uid(userID)
 
     elif flask.request.method == 'GET':
-        return get_address_by_uid(request.args.get('userID'))
+        return get_address_by_uid(userID)
 
 
 @app.route('/address', methods=['GET', 'POST'])
@@ -61,26 +60,25 @@ def address():
 
 
 @app.route('/address/<addressID>', methods=['GET', 'PUT', 'DELETE'])
-def address_id():
+def address_id(addressID):
     if flask.request.method == 'GET':
-        return get_address_by_aid(request.args.get('addressID'))
+        return get_address_by_aid(addressID)
 
     elif flask.request.method == 'PUT':
-        update_address(request.args.get('addressID'), request.form['address'])
+        update_address(addressID, request.form['address'])
 
     elif flask.request.method == 'DELETE':
-        delete_address(request.args.get('addressID'))
+        delete_address(addressID)
 
 
 @app.route('/address/<addressID>/users', methods=['GET', 'POST'])
-def address_id_users():
+def address_id_users(addressID):
     if flask.request.method == 'POST':
-        return insert_user_by_addressid(request.args.get())
+        return insert_user_by_addressid(addressID)
 
     elif flask.request.method == 'GET':
-        return get_user_by_addressid(request.args.get())
+        return get_user_by_addressid(addressID)
 
 
 if __name__ == '__main__':
     app.run()
-
