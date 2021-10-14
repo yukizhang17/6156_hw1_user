63f85f5c606cccb0c12d597fe6ef2c9e73c851de
Shihan Cheng <88729295+s7prime@users.noreply.github.com>
2021-10-14 01:14:13 -0400

    Add files via upload
    updated address_service.py and base_application_resource.py

---
+++ base_application_resource.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+from database_services.base_rdb_service import BaseDataResource
+
+class BaseApplicationException(Exception):
+
+    def __init__(self):
+        pass
+
+
+class BaseApplicationResource(ABC):
+
+    def __init__(self):
+        pass
+
+    def get_links(cls, resource_data):
+        for resource in resource_data:
+            address_id = resource.get('id')
+
+            links = []
+            address_link = {"rel": "self", "href": "/addresses/" + str(address_id)}
+            links.append(address_link)
+
+            users_link = {"rel": "users", "href": "/users?address_id=" + str(address_id)}
+            links.append(users_link)
+
+            resource['links'] = links
+            return resource_data
+
+    @classmethod
+    def get_by_template(self, db_name, table_name, template):
+        resource = BaseDataResource.find_by_template(db_name, table_name, template)
+        # result = self.get_links(resource)
+        print(resource)
+        return resource
+
+
+
+    @classmethod
+    def get_by_resource_id(cls, key_values, field_list):
+        pass
+
+    @classmethod
+    def create(db_name, table_name, create_data):
+        res = BaseDataResource.create(db_name, table_name, create_data)
+
+        return res
+
+    @classmethod
+    def update(db_name, table_name, update_data, template):
+        res = BaseDataResource.update(db_name, table_name, update_data, template)
+        return res
+
+    @classmethod
+    def delete(db_name, table_name, template):
+        res = BaseDataResource.delete(db_name, table_name, template)
+        return res
+
+    @classmethod
+    @abstractmethod
+    def get_links(cls, resource_data):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_data_resource_info(cls):
+        pass
+
+
